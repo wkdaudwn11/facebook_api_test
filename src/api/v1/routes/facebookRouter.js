d67e72ce02d447d06f4adef7d5596a54c4e83987
JangMyungJoo <wkdaudwn1028@gmail.com>
2020-03-26 17:46:24 +0900

    애드셋, 애드 목록 API 추가

--- src/api/v1/routes/facebookRouter.js
+++ src/api/v1/routes/facebookRouter.js
@@ -4,17 +4,24 @@ const router = express.Router();
 const facebookController = require("api/v1/controllers/facebookController.js");
 
 /**
- * @route  GET api/v1/facebook/campaign-list
+ * @route  GET api/v1/facebook/campaign/list
  * @desc   캠페인 목록 불러오기
  * @access Public
  */
-router.route("/campaign-list").get(facebookController.getCampaignList);
+router.route("/campaign/list").get(facebookController.getCampaignList);
 
 /**
- * @route  GET api/v1/facebook/ad-account
- * @desc   광고 계정 정보 불러오기
+ * @route  GET api/v1/facebook/adset/list
+ * @desc   애드셋 목록 불러오기
  * @access Public
  */
-router.route("/ad-account").get(facebookController.getAdAccount);
+router.route("/adset/list").get(facebookController.getAdSetList);
+
+/**
+ * @route  GET api/v1/facebook/ad/list
+ * @desc   애드 목록 불러오기
+ * @access Public
+ */
+router.route("/ad/list").get(facebookController.getAdList);
 
 module.exports = router;
